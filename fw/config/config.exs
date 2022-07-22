3905ac06e8be2a4ba03490770cb6eb2f3057e11a
Nick Veys <nick@elixir.blue>
2022-07-22 01:17:23 -0500

    Update Nerves

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -1,69 +1,36 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# This file is responsible for configuring your application and its
+# dependencies.
 #
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-use Mix.Config
+# This configuration file is loaded before any dependency and is restricted to
+# this project.
+import Config
 
 # Blinkchain-specific configuration
 import_config "blinkchain.exs"
+
+# Enable the Nerves integration with Mix
+Application.start(:nerves_bootstrap)
+
+config :rpi0_test, target: Mix.target()
 
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
-# Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
 
-config :shoehorn,
-  init: [:nerves_runtime, :nerves_init_gadget],
-  app: Mix.Project.config()[:app]
-
-key = Path.join(System.user_home!(), ".ssh/id_rsa.pub")
-unless File.exists?(key), do: Mix.raise("No SSH Keys found. Please generate an ssh key")
-
-config :nerves_firmware_ssh,
-  authorized_keys: [
-    File.read!(key)
-  ]
+config :nerves, source_date_epoch: "1658034464"
 
 # Use Ringlogger as the logger backend and remove :console.
 # See https://hexdocs.pm/ring_logger/readme.html for more information on
 # configuring ring_logger.
 
-config :logger, backends: [RingLogger], level: :debug
+config :logger, backends: [RingLogger]
 
-# Configure nerves_init_gadget.
-# See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
-
-# Setting the node_name will enable Erlang Distribution.
-# Only enable this for prod if you understand the risks.
-node_name = if Mix.env() != :prod, do: "fw"
-
-config :nerves_init_gadget,
-  ifname: "eth0",
-  address_method: :dhcp,
-  mdns_domain: "nerves.local",
-  node_name: node_name,
-  node_host: :mdns_domain
-
-# Configure Phoenix application (see wiki)
-config :ui, UIWeb.Endpoint,
-  url: [host: "localhost"],
-  http: [port: 80],
-  secret_key_base: "W6cRhNEDT4pvEphRZIt3IXXGctpEx1HxZm6cXE1wMtTFq0HJ107LPry71hnXL9Yj",
-  root: Path.dirname(__DIR__),
-  server: true,
-  render_errors: [view: UIWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: UI.PubSub, adapter: Phoenix.PubSub.PG2],
-  code_reloader: false
-
-config :phoenix, :json_library, Jason
-
-# Import target specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-# Uncomment to use target specific configurations
-
-# import_config "#{Mix.target()}.exs"
+if Mix.target() == :host do
+  import_config "host.exs"
+else
+  import_config "target.exs"
+end
--- fw/mix.exs
+++ fw/mix.exs
@@ -1,33 +1,29 @@
 defmodule FW.MixProject do
   use Mix.Project
 
-  @all_targets [:host, :rpi2]
+  @app :rpi0_test
+  @version "0.1.0"
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64, :grisp2]
 
   def project do
     [
-      app: :fw,
-      version: "0.1.0",
-      elixir: "~> 1.6",
-      archives: [nerves_bootstrap: "~> 1.0"],
+      app: @app,
+      version: @version,
+      elixir: "~> 1.11",
+      archives: [nerves_bootstrap: "~> 1.11"],
       start_permanent: Mix.env() == :prod,
-      build_embedded: Mix.target() != :host,
-      aliases: [loadconfig: [&bootstrap/1]],
-      deps: deps()
+      build_embedded: true,
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host]
     ]
-  end
-
-  # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
-  # Aliases are only added if MIX_TARGET is set.
-  def bootstrap(args) do
-    Application.start(:nerves_bootstrap)
-    Mix.Task.run("loadconfig", args)
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {FW.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:inets, :logger, :runtime_tools]
     ]
   end
 
@@ -35,25 +31,46 @@ defmodule FW.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.4", runtime: false},
-      {:shoehorn, "~> 0.4"},
-      {:ring_logger, "~> 0.6"},
-      {:toolshed, "~> 0.2"},
-      {:color_stream, "~> 0.0"},
-      {:blinkchain, "~> 1.0.0-rc0"},
-      {:ui, path: "../ui"},
+      {:nerves, "~> 1.7.16 or ~> 1.8.0", runtime: false},
+      {:shoehorn, "~> 0.9.1"},
+      {:ring_logger, "~> 0.8.5"},
+      {:toolshed, "~> 0.2.26"},
+
+      {:color_stream, "~> 0.0.0"},
+      {:blinkchain, "~> 1.0.0"},
+      # {:blinkchain, path: "../blinkchain.git"},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.6", targets: @all_targets},
+      {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
+      {:nerves_pack, "~> 0.7.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.5", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.5", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.5", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.5", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.5", runtime: false, targets: :rpi3a},
-      {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.5", runtime: false, targets: :x86_64}
+      # NOTE: It's generally low risk and recommended to follow minor version
+      # bumps to Nerves systems. Since these include Linux kernel and Erlang
+      # version updates, please review their release notes in case
+      # changes to your application are needed.
+      # {:nerves_system_rpi, "~> 1.19", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.19", runtime: false, targets: :rpi0},
+      # {:nerves_system_rpi2, "~> 1.19", runtime: false, targets: :rpi2},
+      # {:nerves_system_rpi3, "~> 1.19", runtime: false, targets: :rpi3},
+      # {:nerves_system_rpi3a, "~> 1.19", runtime: false, targets: :rpi3a},
+      # {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4},
+      # {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
+      # {:nerves_system_osd32mp1, "~> 0.10", runtime: false, targets: :osd32mp1},
+      # {:nerves_system_x86_64, "~> 1.19", runtime: false, targets: :x86_64},
+      # {:nerves_system_grisp2, "~> 0.3", runtime: false, targets: :grisp2}
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      # Erlang distribution is not started automatically.
+      # See https://hexdocs.pm/nerves_pack/readme.html#erlang-distribution
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod or [keep: ["Docs"]]
     ]
   end
 end
