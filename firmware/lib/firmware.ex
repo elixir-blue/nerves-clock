fd6edeb2a502cdb7891db7887b297f5dde40716c
Nick Veys <nick@elixir.blue>
2022-07-29 17:17:13 -0500

    Clean up tests

--- firmware/lib/firmware.ex
+++ firmware/lib/firmware.ex
@@ -1,18 +1,3 @@
 defmodule Firmware do
-  @moduledoc """
-  Documentation for Firmware.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Firmware.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
